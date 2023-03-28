116ec4bd926460997356b0bb2082728f9afa71b6
Fernando <ferobregon03@gmail.com>
2023-03-29 00:06:30 +0100

    Added CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://feernandooff.com",
   integrations: [mdx(), sitemap(), svelte(), tailwind()],
   vite: {
     ssr: {
