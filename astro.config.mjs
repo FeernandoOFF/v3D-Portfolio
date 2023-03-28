839d3e05f597f5c72f87106b29e9ae7a3ad319a2
Fernando <ferobregon03@gmail.com>
2023-03-28 22:51:01 +0100

    Initial setup: Svelte,Tailwind, Threlte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 
-import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), svelte(), tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ["three", "troika-three-text"],
+    },
+  },
 });
