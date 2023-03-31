4d39df0aadce2a9fb6bd85c75a17706e56e477e8
Fernando <ferobregon03@gmail.com>
2023-03-31 20:16:10 +0100

    added services screen and snippets folder

---
+++ src/components/ServicesScreen.svelte
@@ -0,0 +1,11 @@
+<div>
+  <div class="mx-auto text-center">
+    <h3>Services</h3>
+    <h2
+      class="font-serif text-xl md:text-4xl mt-2 mb-8"
+      style="-webkit-text-stroke: 1px; -webkit-text-fill-color: transparent;"
+    >
+      360 Solutions
+    </h2>
+  </div>
+</div>
