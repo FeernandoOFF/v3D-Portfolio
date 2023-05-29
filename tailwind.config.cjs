908bf4215228fec79f7478a3bcda42b660e41f0e
Fernando <ferobregon03@gmail.com>
2023-05-29 21:13:48 +0100

    [Updated] Mainscreen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'sans': ['Montserrat', ...defaultTheme.fontFamily.sans],
+				'quick-sand': ['Quicksand', ...defaultTheme.fontFamily.sans],
+			}
+		},
 	},
 	daisyui: {
 		themes: [
@@ -34,5 +41,5 @@ module.exports = {
 			}
 		]
 	},
-	plugins: [require("daisyui")],
+	plugins: [require("daisyui"), require('@tailwindcss/typography')],
 }
